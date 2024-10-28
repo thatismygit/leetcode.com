09e39a68595e8cd5714e5eb8713f73e622d42002
= <=>
2024-10-28 16:02:58 +0530

    appending 204 file

--- 204. Count Primes .py
+++ 204. Count Primes .py
@@ -1,14 +1,27 @@
+# class Solution:
+#     def countPrimes(self, n: int) -> int:
+#         ans=0
+#         record=list(True for i in range(n))
+#         p=2
+#         while p*p<n:
+#             if record[p]:
+#                 for i in range(p*p,n,p):
+#                     record[i]=False
+#             p+=1
+#         for i in range(2,n):
+#             if record[i]:
+#                 ans+=1
+#         return ans
+
 class Solution:
     def countPrimes(self, n: int) -> int:
-        ans=0
-        record=list(True for i in range(n))
+        if not n or n==1:
+            return 0
+        record=list(1 for i in range(n))
         p=2
         while p*p<n:
             if record[p]:
                 for i in range(p*p,n,p):
-                    record[i]=False
+                    record[i]=0
             p+=1
-        for i in range(2,n):
-            if record[i]:
-                ans+=1
-        return ans
+        return sum(record)-2
