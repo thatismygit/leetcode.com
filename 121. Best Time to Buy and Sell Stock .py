01f9175ca09a4d9c829564d7dfebb07190c0048f
Vishnu Pandey <vishnupandey082@gmail.com>
2024-12-02 04:58:06 +0530

    two pointer question

--- 121. Best Time to Buy and Sell Stock .py
+++ 121. Best Time to Buy and Sell Stock .py
@@ -1,11 +1,24 @@
 # 46 ms
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         max_profit=0
+#         previous=prices[0]
+#         for i in range(len(prices)):
+#             if max_profit<(prices[i]-previous):
+#                 max_profit=prices[i]-previous
+#             if previous>prices[i]:
+#                 previous=prices[i]
+#         return max_profit
+
+# 33 ms
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        max_profit=0
-        previous=prices[0]
-        for i in range(len(prices)):
-            if max_profit<(prices[i]-previous):
-                max_profit=prices[i]-previous
-            if previous>prices[i]:
-                previous=prices[i]
-        return max_profit
+        if len(prices)==1: return 0
+        ans=0
+        low=0
+        for high in range(1,len(prices)):
+            if ans<(prices[high]-prices[low]):
+                ans=prices[high]-prices[low]
+            if prices[low]>prices[high]:
+                low=high
+        return ans
